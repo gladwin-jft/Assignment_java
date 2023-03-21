8ac755837463c283fcd86eb6c30e791d768d8aa1
gladwin-jft <128125104+gladwin-jft@users.noreply.github.com>
2023-03-21 20:55:14 +0530

    Ques two

---
+++ Ques_two.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+public class Ques_two 
+{
+    public static void main(String[] args)
+    {
+        //Define a class which has a main method, it initializes 
+        // an integer array , display its element and it sum
+
+        int [] arr = {34, 2, 1, -8, 48, -12, 7};
+
+        System.out.print("Element in an array: ");
+        for(int n : arr) System.out.print(n+ " ");
+
+        int sum = Arrays.stream(arr).sum();
+
+        /*for(int i = 0 ; i < arr.length ; i++)
+        {   
+            sum += arr[i]
+        }
+        System.out.println("\nsum of array is " + sum);*/
+
+        
+        System.out.println("\nsum of array is " + sum);
+    
+
+
+    }
+}
