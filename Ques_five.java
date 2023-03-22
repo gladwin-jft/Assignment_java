6bfd58986f9ab01e2e750763cebc6fb4e04a547b
gladwin-jft <128125104+gladwin-jft@users.noreply.github.com>
2023-03-22 06:26:32 +0530

    Add files via upload

--- Ques_five.java
+++ Ques_five.java
@@ -1,45 +1,20 @@
-import java.util.Random;
-public class Ques_five {
-    public static void main(String args[])
+public class Ques_five
+{
+    public static void main(String[] args)
     {
-        // Define a class which has a main method
-        //it creates an integer array of 10 elements
-        // it initializes its values from 1 to 10
-        int [] nums = new int [10];
+        //Define a class which has a main method 
+        //it initializes two integers 
+        //displays their values and swaps
+        //them without using a third integer
 
-        for(int i = 1 ; i <= 10 ; i++)
-            nums[i-1] = i;
+        int num1 = 45 , num2 = 54;
 
-        Random random = new Random();
-        int rand_num = random.nextInt(10);
+        System.out.println("Before Swap " + num1 + ", "+ num2);
 
-        nums[rand_num] = 0;
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
 
-        find_missing_element(nums);
-        
-        
+        System.out.println("After Swap " + num1 + ", "+ num2);
     }
-    public static void find_missing_element(int [] nums)
-    {
-        //Your method should identify and display the missing value
-    
-
-        for(int i = 0 ; i < nums.length ; i++)
-        {
-            if(nums[i] == 0)
-                System.out.println("The missing element is " + (i + 1));
-        }
-        display(nums);
-
-    }
-    public static void display(int [] arr)
-    {
-        for(int i = 0 ; i < arr.length ; i++)
-        {
-            System.out.print(arr[i] + " ,");
-        }
-        System.out.println("");
-    }
-    
-    
 }
