0d22a239c5d162243f0608e6b1d65593ce403752
gladwin-jft <128125104+gladwin-jft@users.noreply.github.com>
2023-03-22 11:42:53 +0530

    Question 6

--- Ques_six.java
+++ Ques_six.java
@@ -6,8 +6,11 @@ public class Ques_six {
     
     public static void main(String[] args) 
     {
-		//HashMap<Integer, Integer> hash_map = new HashMap<>();
-        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		/*
+			Define a class which has a main method, It creates an integer array of 10 elements and initializes it by values from 1 to 10.  
+			Replace a random element value by 0. Your method should identify and display the missing element ( which is replaced by zero).
+		*/
+        	int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
 		int sum = (array.length / 2) * (array.length + 1);
 		
